d4b23e5573ecb6b8916462c2e56e897e5385dc97
daniel.foulkes <Daniel.Foulkes@ee.co.uk>
2019-12-09 23:22:38 +0000

    OpenTycoon-1 Core - Game Time asdding the

---
+++ game/core/src/test/java/uk/co/opentycoon/CucumberTests.java
@@ -0,0 +1,21 @@
+package uk.co.opentycoon;
+
+import static org.junit.Assert.assertTrue;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Unit test for simple App.
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class CucumberTests {
+
+
+    public static void main(String[] args){
+
+    }
+}
